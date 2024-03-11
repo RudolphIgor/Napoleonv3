f989d0843e32e0697ad54a84b99f92b7baf71b00
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-03-11 11:35:27 +0700

    Добавлен компонент FOOTER

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <div className="wrapper">
-
+            <Header/>
             <BrowserRouter>
 
             </BrowserRouter>
-
+            <Footer/>
         </div>
     );
 }
