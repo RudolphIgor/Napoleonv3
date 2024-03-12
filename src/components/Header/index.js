fa27132fda217eb9ff7a9b99459358d1340daeae
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-03-12 09:53:50 +0700

    Массив с переменными меню, вынесен в отдельный файл

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,39 +4,11 @@ import style from "./index.module.css"
 import Social from "./Social";
 import Contacts from "./Contacts";
 import Nav from "./Nav";
+import {menuItems} from "../../data/menuItems"
 
 
 const Index = () => {
-    //Массив пунктов меню
-    const menuItems = [
-        {
-            id: 1,
-            title: "Главная",
-            route: '/'
-        },
-        {
-            id: 2,
-            title: "Каталог",
-            route: '/'
-        },
-        {
-            id: 3,
-            title: "О нас",
-            route: '/'
-        },
-        {
-            id: 4,
-            title: "Акции",
-            route: '/'
-        },
-        {
-            id: 5,
-            title: "Отзывы",
-            route: '/'
-        },
-    ]
-
-    return (
+        return (
         <header className={style.header__container}>
             <Social/>
             <Logo className={style.logo} />
---
+++ src/data/menuItems.js
@@ -0,0 +1,27 @@
+export const menuItems = [
+    {
+        id: 1,
+        title: "Главная",
+        route: '/'
+    },
+    {
+        id: 2,
+        title: "Каталог",
+        route: '/'
+    },
+    {
+        id: 3,
+        title: "О нас",
+        route: '/'
+    },
+    {
+        id: 4,
+        title: "Акции",
+        route: '/'
+    },
+    {
+        id: 5,
+        title: "Отзывы",
+        route: '/'
+    }
+]
