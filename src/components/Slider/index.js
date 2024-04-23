bcebaf918af2fb4287308cccc7283daf1f4504f7
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-04-23 09:39:09 +0700

    Добавлено модальное окно в виде портала

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './index.module.css'
+import Modal from "../Modal";
 
 const Index = (props) => {
     const styleblock = {
         background: `url(${props.slideArray.img}) center no-repeat`,
         height: "clamp(141px, 41.5vw, 498px)",
-
     }
+    const [isOpen, setIsOpen] = useState(false);
+    console.log(`isOpen: ${isOpen}`);
     return (
         <div style={styleblock}>
             <div className={style.content}>
                 <p className={style.title}>
                     {props.slideArray.text}
                 </p>
-                <button className={style.button}>
+                <button className={style.button} onClick={() => setIsOpen(true)}>
                     {props.slideArray.text_btn}
                 </button>
+                {isOpen && (
+                    <Modal>
+                        <p>Some Text</p>
+                    </Modal>
+                )}
             </div>
         </div>
     );
