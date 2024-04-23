c9d663eaea7622cd270efbe1a8b2ca81c24f5d9e
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-04-23 09:28:07 +0700

    Добавлен портал вроде как

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {ReactComponent as IconClose} from "../../img/iconClose.svg";
-
+import ReactDOM from "react-dom";
 import style from './index.module.css';
 import clsx from "clsx";
 
-const Index = ({active, setActive}) => {
-    return (
-        <div className={active ? clsx(style.modal, style.active) : clsx(style.modal)} onClick={() => setActive(false)}>
-            <div className={clsx(style.modalContent)} onClick={e => e.stopPropagation()}>
+
+const Index = ({children}) => {
+    return ReactDOM.createPortal(
+        <div className={clsx(style.modal)}>
+            <div className={clsx(style.modalContent)}>
                 <button className={clsx(style.modalCloseButton)}>
                     <IconClose/>
+                    {children}
                 </button>
             </div>
-        </div>
+        </div>,
+        document.getElementById('modal-root')
     );
 };
 
